76f42e8da351d5908935ce513381ac2ecb47b29d
Emilio <epelaaez@gmail.com>
2021-03-09 13:52:26 +0100

    add blog homepage

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Me',
+        'title': 'Blog Post',
+        'content': 'Content Test',
+        'date_posted': 'March 9, 2021'
+    },
+    {
+        'author': 'You',
+        'title': 'Second Blog Post',
+        'content': 'Second Content Test',
+        'date_posted': 'February 22, 2021'
+    },
+]
+
 def home(request):
     """
     Homepage for blog app.
     """
-    return HttpResponse('<h1>Test - Home Blog</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
