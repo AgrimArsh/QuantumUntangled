427a13862e72e82e1922b7cb822656bb1b03cac6
Emilio <epelaaez@gmail.com>
2021-03-10 16:19:02 +0100

    add article tags

--- website/blog/views.py
+++ website/blog/views.py
@@ -6,12 +6,14 @@ posts = [
         'author': 'Me',
         'title': 'Blog Post',
         'content': 'Content Test',
+        'tags': ['Physics', 'Mathematics', 'Computer Science'],
         'date_posted': 'March 9, 2021'
     },
     {
         'author': 'You',
         'title': 'Second Blog Post',
         'content': 'Second Content Test',
+        'tags': ['Mathematics', 'Quantum Computing'],
         'date_posted': 'February 22, 2021'
     },
 ]
