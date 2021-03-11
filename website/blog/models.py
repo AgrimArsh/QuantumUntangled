d0e240762a7e0b89c8904d10f209b96d4a5d051c
Emilio <epelaaez@gmail.com>
2021-03-11 14:37:04 +0100

    start database and switch dummy data

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__():
+        return f'{self.name}'
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f'{self.title} by {self.author.username}'
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,29 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Me',
-        'title': 'Blog Post',
-        'content': 'Content Test',
-        'tags': ['Physics', 'Mathematics', 'Computer Science'],
-        'date_posted': 'March 9, 2021'
-    },
-    {
-        'author': 'You',
-        'title': 'Second Blog Post',
-        'content': 'Second Content Test',
-        'tags': ['Mathematics', 'Quantum Computing'],
-        'date_posted': 'February 22, 2021'
-    },
-]
+from .models import Article
 
 def home(request):
     """
     Homepage for blog app.
     """
     context = {
-        'posts': posts
+        'posts': Article.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
