3e1f7fe6efa67ec37b18f67216b659b7e3b74ad0
Emilio <epelaaez@gmail.com>
2021-04-04 23:09:00 -0500

    add categories index and category specific pages

--- website/blog/models.py
+++ website/blog/models.py
@@ -16,11 +16,12 @@ from wagtail.images.blocks import ImageChooserBlock
 # Wagtail tagging
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.contrib.taggit import ClusterTaggableManager
-from taggit.models import TaggedItemBase\
+from taggit.models import TaggedItemBase
 
 # Wagtail snippets
 from wagtail.snippets.models import register_snippet
 
+#################### Wagtail models ####################
 # Home page
 class HomePage(Page):
     body = RichTextField(blank=True)
@@ -40,8 +41,8 @@ class PostIndexPage(Page):
     def get_context(self, request):
         # Include only published posts, ordered by reverse-chron
         context = super().get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
-        context['blogpages'] = blogpages
+        postpages = self.get_children().live().order_by('-first_published_at')
+        context['postpages'] = postpages
         return context
 
 # Tag model for tagging posts. Authors can add tags to their posts.
@@ -64,11 +65,11 @@ class PostTagIndexPage(Page):
         if request.GET.get('tag'):
             # Filter by tag
             tag = request.GET.get('tag')
-            blogpages = PostPage.objects.live().filter(tags__name=tag)
+            postpages = PostPage.objects.live().filter(tags__name=tag)
 
             # Update template context
             context = super().get_context(request)
-            context['blogpages'] = blogpages
+            context['postpages'] = postpages
         else: 
             tags = []
             post_pages = PostPage.objects.live()
@@ -110,13 +111,7 @@ class PostPage(Page):
         StreamFieldPanel('body', classname="full"),
     ]
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-
-    def __str__(self):
-        return f'{self.user}'
-
+# Categorize posts
 @register_snippet
 class PostCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -131,7 +126,45 @@ class PostCategory(models.Model):
     class Meta:
         verbose_name_plural = 'post categories'
 
+# Show posts by category 
+class PostCategoryIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+    def get_context(self, request):
+        if request.GET.get('category'):
+            # Filter by category
+            category = request.GET.get('category')
+            postpages = PostPage.objects.live().filter(categories__name=category)
+
+            # Update template context
+            context = super().get_context(request)
+            context['postpages'] = postpages
+        else: 
+            categories = []
+            post_pages = PostPage.objects.live()
+            for page in post_pages:
+                categories += page.categories.all()
+            categories = sorted(set(categories))
+
+            # Update template context
+            context = super().get_context(request)
+            context['categories'] = categories
+        
+        return context
+
+#################### Native Django models ####################
 # Create Profile when creating user (https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#disqus_thread)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return f'{self.user}'
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
